8885718c279b4eac5047b2c85c9481179460097e
AlSurick <surickal@yandex.ru>
2021-07-14 12:09:14 +0300

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static double Calculate(double operand1, double operand2)
+        {
+            return 0.0
+        }
+    }
+}
